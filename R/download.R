5a213b062f34a4e81e2669729f406153b3bc4d93
Julio Trecenti <julio.trecenti@gmail.com>
2018-02-13 19:10:24 -0200

    add jid to params, https and remove possibly from functions

--- R/download.R
+++ R/download.R
@@ -1,9 +1,15 @@
-
 # [Safe] Rerun search for a page os results
-pagination <- purrr::possibly(function(trt, path, booklet, dates, page, state) {
+pagination <- function(page, trt, path, booklet, dates, state, jid) {
+
+  page <- (page - 1) * 30 + 1
 
   # Create body of POST request
   body <- list(
+    'corpo:formulario:botaoAcaoRecuperaPorDemanda' = '',
+    'corpo:formulario:botaoAcaoLimparArgs_LIMPAR_ARGS' = '',
+    'corpo:formulario:botaoAcaoPesquisar' = '',
+    'corpo:formulario:botaoRecuperaUnidadePorTribunalSelecionado' = '',
+    "corpo:formulario:confirma" = "",
     "corpo:formulario:dataIni" = format(dates[1], "%d/%m/%Y"),
     "corpo:formulario:dataFim" = format(dates[2], "%d/%m/%Y"),
     "corpo:formulario:tipoCaderno" = booklet,
@@ -18,26 +24,30 @@ pagination <- purrr::possibly(function(trt, path, booklet, dates, page, state) {
     "org.apache.myfaces.trinidad.faces.FORM" = "corpo:formulario",
     "_noJavaScript" = "false",
     "javax.faces.ViewState" = state,
-    "source" = "corpo:formulario:botaoAcaoPesquisar")
+    "event" = "autosub",
+    "source" = paste0("corpo:formulario:", jid),
+    "partial" = "true")
 
   # Get search results
   r_djt <- httr::POST(
-    "http://dejt.jt.jus.br/dejt/f/n/diariocon",
+    "https://dejt.jt.jus.br/dejt/f/n/diariocon",
     body = body, encode = 'form')
+  # scrapr::html_view(r_djt)
 
   # Parse results table and get new state
   djt_table <- parse_table(r_djt)
   state <- get_state(r_djt)
+  jid <- pdf_jid(r_djt)
 
   # Download PDFs
-  download_pdfs(trt, path, booklet, dates, page, state, djt_table)
+  download_pdfs(trt, path, booklet, dates, page, state, jid, djt_table)
 
-}, tibble::tibble(file = "", result = "error"))
+}
 
 # Download PDFs of a page
-download_pdfs <- function(trt, path, booklet, dates, page, state, djt_table) {
+download_pdfs <- function(trt, path, booklet, dates, page, state, jid, djt_table) {
 
-  # Sabe table as an RDS
+  # Save table as an RDS
   page_i <- stringr::str_pad((page-1)/30+1, 5, "left", "0")
   readr::write_rds(djt_table, stringr::str_c(path, "/table_page_", page_i, ".rds"))
 
@@ -46,14 +56,14 @@ download_pdfs <- function(trt, path, booklet, dates, page, state, djt_table) {
     dplyr::mutate(id = (1:n()) - 1L) %>%
     dplyr::group_by(id, title, date) %>%
     dplyr::do(download_pdf(
-      trt, path, booklet, dates, page, state,
+      trt, path, booklet, dates, page, state, jid,
       .$form, .$id, .$date, .$title)) %>%
     dplyr::ungroup()
 }
 
 # [Safe] Download a PDF
 download_pdf <- purrr::possibly(function(trt, path, booklet, dates, page, state,
-                                         form, id, date, title) {
+                                         jid, form, id, date, title) {
 
   # Create indexes for file name
   page_i <- stringr::str_pad((page-1)/30+1, 5, "left", "0")
@@ -85,11 +95,11 @@ download_pdf <- purrr::possibly(function(trt, path, booklet, dates, page, state,
       "org.apache.myfaces.trinidad.faces.FORM" = "corpo:formulario",
       "_noJavaScript" = "false",
       "javax.faces.ViewState" = state,
-      "source" = sprintf("corpo:formulario:plcLogicaItens:%d:j_id135", id))
+      "source" = sprintf("corpo:formulario:plcLogicaItens:%d:%s", id, jid))
 
     # Download file
     httr::POST(
-      "http://dejt.jt.jus.br/dejt/f/n/diariocon",
+      "https://dejt.jt.jus.br/dejt/f/n/diariocon",
       body = body, httr::write_disk(file, TRUE))
   }
 
